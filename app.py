052b6ea14bae5a0d59a6b736730fb2b01346cce9
Tanishka Bajpai <your-email@example.com>
2026-04-21 02:18:20 +0530

    Fixed app for deployment

--- app.py
+++ app.py
@@ -33,5 +33,7 @@ def predict():
         "predicted_class": int(np.argmax(prediction))
     })
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
